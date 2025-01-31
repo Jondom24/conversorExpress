958c42a15f202423b112a76765bd4c3702df82a3
Jonathan Ruiz <jonathandominikruiz@gmail.com>
2025-01-31 17:51:41 -0500

    correccion

--- conexion.js
+++ conexion.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2/promise');
 // Create the connection to database
+//mysql://root:CGWQbZLEzeqwmkADikxwTJvCaaBhMPxl@viaduct.proxy.rlwy.net:26048/railway
 const connection = mysql.createPool({
     host: process.env.HOSTDB || 'localhost',
-    user: process.env.HOSTDB || 'root',
+    user: process.env.USERDB || 'root',
     database: process.env.DB || 'servletlogin',
     password: process.env.PASSWORDDB || '',
     port: process.env.PORTDB || 3306,
